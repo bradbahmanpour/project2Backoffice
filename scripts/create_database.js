152e2469952d36732e28570b29055f0f07470cab
Brad Bahmanpour <bradbahmanpour@Brads-MacBook-Pro.local>
2018-04-03 19:21:21 -0700

    Added Heroku related SQL production settings

--- scripts/create_database.js
+++ scripts/create_database.js
@@ -1,11 +1,15 @@
-/**
- * Created by barrett on 8/28/14.
- */
-
 var mysql = require('mysql');
 var dbconfig = require('../config/database');
 
-var connection = mysql.createConnection(dbconfig.connection);
+var coonection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+    connection = mysql.createConnection(dbconfig.connection);
+  };
+
+// var connection = mysql.createConnection(dbconfig.connection);
 
 connection.query('CREATE DATABASE ' + dbconfig.database);
 
