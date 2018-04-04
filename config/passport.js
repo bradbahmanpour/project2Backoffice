cb33b2dc2a7a1c05857b284c478d1365c738439a
Brad Bahmanpour <bradbahmanpour@Brads-MacBook-Pro.local>
2018-04-03 23:35:29 -0700

    Added Heroku related SQL statements to passport.js

--- config/passport.js
+++ config/passport.js
@@ -6,10 +6,25 @@ var LocalStrategy   = require('passport-local').Strategy;
 // load up the user model
 var mysql = require('mysql');
 var bcrypt = require('bcrypt-nodejs');
-var dbconfig = require('./database');
-var connection = mysql.createConnection(dbconfig.connection);
 
-connection.query('USE ' + dbconfig.database);
+
+// var dbconfig = require('./database');
+// var connection = mysql.createConnection(dbconfig.connection);
+
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "dive_db"
+  });
+};
+
+// connection.query('USE ' + dbconfig.database);
 // expose this function to our app using module.exports
 module.exports = function(passport) {
 
